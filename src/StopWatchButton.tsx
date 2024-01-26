01219096d19635648c52d4f08ab0f8f56d0f032b
adrienbelcastro <adrienbelcastro1@gmail.com>
2024-01-26 12:18:39 -0500

    created button component & passed props to it

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./stopwatch.css";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string;
+}
+
+export default function StopWatchButton({
+  label,
+  onClick,
+  className,
+}: ButtonProps) {
+  console.log(onClick);
+  return (
+    <button onClick={onClick} className={className}>
+      {label}
+    </button>
+  );
 }
